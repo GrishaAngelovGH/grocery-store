e6a4a22a445985d43e0fc85ebbc2c503f99cba7a
Grisha Angelov <grisha.angelov89@gmail.com>
2020-12-08 12:30:56 +0200

    Implemented Menu with categories and subCategories for HomePage.

---
+++ src/components/Pages/HomePage/Menu/Menu.js
@@ -0,0 +1,67 @@
+import { Component } from 'react'
+import PropTypes from 'prop-types'
+
+import './Menu.scss'
+
+class Menu extends Component {
+    state = { subCategories: [] }
+
+    handleMouseEnter = ({ target }) => {
+        const title = target.id
+        const subCategories = this.props.categories.find(v => v.title === title).subCategories
+        this.setState({ subCategories })
+    }
+
+    render() {
+        const { mobile, categories } = this.props
+        const { subCategories } = this.state
+
+        return (
+            <div className='row menu-container'>
+                <div className='col-md-12'>
+                    <nav className='navbar navbar-expand-lg navbar-light'>
+
+                        <button className='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>
+                            <span className='navbar-toggler-icon'></span>
+                        </button>
+
+                        {
+                            !mobile && (
+                                <div className='collapse navbar-collapse justify-content-center' id='navbarNav'>
+                                    <ul className='navbar-nav'>
+                                        {
+                                            categories.map(v => (
+                                                <li key={v.title} className='nav-item' onMouseEnter={this.handleMouseEnter}>
+                                                    <a id={v.title} className='nav-link' href='#'>{v.title}</a>
+                                                </li>
+                                            ))
+                                        }
+                                    </ul>
+                                </div>
+                            )
+                        }
+                    </nav>
+
+                    {
+                        !mobile && (
+                            <div className='row justify-content-center subcategories'>
+                                <div className='col-md-6 d-flex justify-content-between border'>
+                                    {
+                                        subCategories.map(v => <div key={v}>{v}</div>)
+                                    }
+                                </div>
+                            </div>
+                        )
+                    }
+                </div>
+            </div>
+        )
+    }
+}
+
+Menu.propTypes = {
+    mobile: PropTypes.bool,
+    categories: PropTypes.array.isRequired
+}
+
+export default Menu
---
+++ src/components/Pages/HomePage/Menu/Menu.spec.js
@@ -0,0 +1,119 @@
+import Menu from './Menu'
+
+describe('(Component) Menu', () => {
+    it('should render Menu', () => {
+        const wrapper = shallow(
+            <Menu
+                categories={
+                    [
+                        { title: 'Title1', subCategories: ['Category1'] },
+                        { title: 'Title2', subCategories: ['Category2'] }
+                    ]
+                }
+            />
+        )
+
+        expect(wrapper.equals(
+            <div className='row menu-container'>
+                <div className='col-md-12'>
+                    <nav className='navbar navbar-expand-lg navbar-light'>
+
+                        <button className='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>
+                            <span className='navbar-toggler-icon'></span>
+                        </button>
+
+                        <div className='collapse navbar-collapse justify-content-center' id='navbarNav'>
+                            <ul className='navbar-nav'>
+                                <li className='nav-item' onMouseEnter={wrapper.instance().handleMouseEnter}>
+                                    <a id={'Title1'} className='nav-link' href='#'>Title1</a>
+                                </li>
+                                <li className='nav-item' onMouseEnter={wrapper.instance().handleMouseEnter}>
+                                    <a id={'Title2'} className='nav-link' href='#'>Title2</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+
+                    <div className='row justify-content-center subcategories'>
+                        <div className='col-md-6 d-flex justify-content-between border'></div>
+                    </div>
+                </div>
+            </div>
+        )).to.equal(true)
+    })
+
+    it('should render mobile Menu', () => {
+        const wrapper = shallow(
+            <Menu
+                mobile
+                categories={
+                    [
+                        { title: 'Title1', subCategories: ['Category1'] },
+                        { title: 'Title2', subCategories: ['Category2'] }
+                    ]
+                }
+            />
+        )
+
+        expect(wrapper.equals(
+            <div className='row menu-container'>
+                <div className='col-md-12'>
+                    <nav className='navbar navbar-expand-lg navbar-light'>
+
+                        <button className='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>
+                            <span className='navbar-toggler-icon'></span>
+                        </button>
+                    </nav>
+                </div>
+            </div>
+        )).to.equal(true)
+    })
+
+    it('should render subCategories on title hover', () => {
+        const wrapper = shallow(
+            <Menu
+                categories={
+                    [
+                        { title: 'Title1', subCategories: ['Category1'] },
+                        { title: 'Title2', subCategories: ['Category2'] }
+                    ]
+                }
+            />
+        )
+
+        const secondItem = wrapper.find('li').at(1)
+        const event = { target: { id: secondItem.props().children.props.id } }
+
+        secondItem.simulate('mouseEnter', event)
+
+        expect(wrapper.equals(
+            <div className='row menu-container'>
+                <div className='col-md-12'>
+                    <nav className='navbar navbar-expand-lg navbar-light'>
+
+                        <button className='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>
+                            <span className='navbar-toggler-icon'></span>
+                        </button>
+
+                        <div className='collapse navbar-collapse justify-content-center' id='navbarNav'>
+                            <ul className='navbar-nav'>
+                                <li className='nav-item' onMouseEnter={wrapper.instance().handleMouseEnter}>
+                                    <a id={'Title1'} className='nav-link' href='#'>Title1</a>
+                                </li>
+                                <li className='nav-item' onMouseEnter={wrapper.instance().handleMouseEnter}>
+                                    <a id={'Title2'} className='nav-link' href='#'>Title2</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+
+                    <div className='row justify-content-center subcategories'>
+                        <div className='col-md-6 d-flex justify-content-between border'>
+                            <div>Category2</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )).to.equal(true)
+    })
+})
