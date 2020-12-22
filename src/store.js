a40d6d1492cfe1f2b976ceec00e514532b80c187
Grisha Angelov <grisha.angelov89@gmail.com>
2020-12-22 16:28:15 +0200

    Integrate redux axios middleware in store

--- src/store.js
+++ src/store.js
@@ -1,11 +1,21 @@
-import { createStore, combineReducers } from 'redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import axios from 'axios';
+import axiosMiddleware from 'redux-axios-middleware';
 
 import items from './reducers/items'
+
+const client = axios.create({
+    baseURL: 'http://localhost:8080',
+    responseType: 'json'
+})
 
 const store = createStore(
     combineReducers({
         items
-    })
+    }),
+    applyMiddleware(
+        axiosMiddleware(client)
+    )
 )
 
 export default store
