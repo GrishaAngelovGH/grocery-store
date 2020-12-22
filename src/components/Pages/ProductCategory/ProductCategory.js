e6b30a53cee91ca4ff0ec95a6916d69e6eb9efc6
Grisha Angelov <grisha.angelov89@gmail.com>
2020-12-22 16:42:10 +0200

    Fetch all items for given category when ProductCategory is mounted

--- src/components/Pages/ProductCategory/ProductCategory.js
+++ src/components/Pages/ProductCategory/ProductCategory.js
@@ -1,5 +1,18 @@
-const ProductCategory = () => (
-    <div>product category</div>
-)
+import { Component } from 'react'
+class ProductCategory extends Component {
+    componentDidMount() {
+        const { match, fetchAllItemsByCategory } = this.props
+
+        const id = match.params.id
+
+        fetchAllItemsByCategory(id)
+    }
+
+    render() {
+        return (
+            <div>product category</div>
+        )
+    }
+}
 
 export default ProductCategory
--- src/components/Pages/ProductCategory/ProductCategory.spec.js
+++ src/components/Pages/ProductCategory/ProductCategory.spec.js
@@ -1,11 +1,28 @@
 import ProductCategory from './ProductCategory'
 
 describe('(Component) ProductCategory', () => {
-    it('should render component', () => {
-        const wrapper = shallow(<ProductCategory />)
+    let wrapper, fetchAllItemsByCategory
+    const props = { match: { params: { id: 'category-1' } } }
 
+    beforeEach(() => {
+        fetchAllItemsByCategory = sinon.spy()
+
+        wrapper = shallow(
+            <ProductCategory
+                {...props}
+                fetchAllItemsByCategory={fetchAllItemsByCategory}
+            />
+        )
+    })
+
+    it('should render component', () => {
         expect(wrapper.equals(
             <div>product category</div>
         )).to.equal(true)
+    })
+
+    it('should fetch all items by category on mount', () => {
+        fetchAllItemsByCategory.should.been.calledOnce
+        fetchAllItemsByCategory.should.been.calledWith('category-1')
     })
 })
