9ada6d6016798f3c43e74597a8e625734a513569
Grisha Angelov <grisha.angelov89@gmail.com>
2020-12-30 16:20:23 +0200

    Implement Billing step

---
+++ src/components/Pages/Checkout/CheckoutSteps/Billing/Billing.js
@@ -0,0 +1,66 @@
+import { Field } from 'react-final-form'
+
+import RequiredInputField from 'components/Forms/RequiredInputField'
+
+const Billing = () => (
+    <div className='row m-3'>
+        <div className='col-md-12'>
+            <div className='row'>
+                <div className='col-md-12'>
+                    <h1>
+                        Billing
+                    </h1>
+                </div>
+            </div>
+
+            <div className='row mb-2'>
+                <div className='col-md-6 col-lg-3'>
+                    <RequiredInputField name={'firstName'} label={'First Name'} />
+                </div>
+            </div>
+
+            <div className='row mb-2'>
+                <div className='col-md-6 col-lg-3'>
+                    <RequiredInputField name={'lastName'} label={'Last Name'} />
+                </div>
+            </div>
+
+            <div className='row mb-2'>
+                <div className='col-md-6 col-lg-3'>
+                    <RequiredInputField name={'email'} label={'Email'} />
+                </div>
+            </div>
+
+            <div className='row mb-2'>
+                <div className='col-md-6 col-lg-3'>
+                    <RequiredInputField name={'phoneNumber'} label={'Phone'} />
+                </div>
+            </div>
+
+            <div className='row mb-2'>
+                <div className='col-md-6 col-lg-3'>
+                    <RequiredInputField name={'billingAddress'} label={'Billing Addr'} />
+                </div>
+            </div>
+
+            <div className='row mb-2'>
+                <div className='col-md-6 col-lg-3'>
+                    <RequiredInputField name={'postCode'} label={'Post Code'} />
+                </div>
+            </div>
+
+            <div className='row mb-2'>
+                <div className='col-md-6 col-lg-3'>
+                    <label>Country</label>
+                    <Field name='favoriteColor' component='select' className='w-100'>
+                        <option value='bg'>Bulgaria</option>
+                        <option value='uk'>United Kingdom</option>
+                        <option value='de'>Germany</option>
+                    </Field>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default Billing
