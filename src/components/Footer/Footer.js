753d9c55cc50fb5358ab67d36ba42e80e5f55711
Grisha Angelov <grisha.angelov89@gmail.com>
2023-02-24 09:51:21 +0200

    Replace double quotes with single quotes in Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,11 +4,11 @@ import './Footer.scss'
 const Footer = () => (
     <div className='row footer-panel p-5 no-gutters justify-content-center text-center'>
         <div className='col-12 col-md-4 col-lg-3 p-3'>
-            <i className="bi bi-facebook mx-1"></i>
-            <i className="bi bi-instagram mx-1"></i>
-            <i className="bi bi-pinterest mx-1"></i>
-            <i className="bi bi-twitter mx-1"></i>
-            <i className="bi bi-youtube mx-1"></i>
+            <i className='bi bi-facebook mx-1'></i>
+            <i className='bi bi-instagram mx-1'></i>
+            <i className='bi bi-pinterest mx-1'></i>
+            <i className='bi bi-twitter mx-1'></i>
+            <i className='bi bi-youtube mx-1'></i>
         </div>
         <div className='col-6 col-md-3 col-lg-3 p-3'>
             <img src={appMarketplace.appStoreLogo} />
